bf05cb3598bb9f4021b4965fc348b9ec8985e74e
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-30 21:03:09 +0300

    Move auth urls from app urls to project's urls

--- water/urls.py
+++ water/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views 
+from billing import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('billing.urls'))
+    path('', include('billing.urls')),
+    path('signup/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/registration/logout.html'), name='logout'),
 ]
