9ea71b6e4b50be4366eb709d7782e0316abe17ac
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-30 21:02:36 +0300

    Modify files

--- water/settings.py
+++ water/settings.py
@@ -134,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -152,3 +152,4 @@ django_heroku.settings(locals())
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'account_details'
